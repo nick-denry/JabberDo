ef926e2ccc1a9b8ce5d4491e2442b2665b42b6e5
Nick Denry <nick.denry@gmail.com>
2019-04-18 21:10:56 +0300

    Add basic help router

--- routers/help_router.py
+++ routers/help_router.py
@@ -17,5 +17,25 @@ class HelpRouter(BaseRouter):
     def __init__(self, xmpp_message: Message):
         BaseRouter.__init__(self, xmpp_message)
 
-    def route(self, command, sender):
-        pass
+    @property
+    def xmpp_message(self):
+        # Return the same value
+        # as in the base class
+        return super().xmmp_message
+
+    @property
+    def current_jid(self):
+        # Return the same value
+        # as in the base class
+        return super().current_jid
+
+    def route(self, message):
+        self.add_reply_message(_(". - output existing lists"))
+        self.add_reply_message(_(".<list_name> - add or display list, i.e. .tasks. Also makes list \"active\""))
+        self.add_reply_message(_(". - display \"active\" list"))
+        self.add_reply_message(_(".-<list_name> - delete whole list and it's tasks"))
+        self.add_reply_message(" ")
+        self.add_reply_message(_("Any message - adds task to active list"))
+        self.add_reply_message(_("-<number> - delete task <number> from list, i.e. -1 deletes first task."))
+        print(self.reply_message)
+        return self.xmpp_message.reply(self.reply_message).send()
