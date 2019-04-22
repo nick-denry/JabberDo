611182bd2823bf1d4fbe86dea6a51f07e26dc82d
Nick Denry <nick.denry@gmail.com>
2019-04-22 22:57:12 +0300

    Update list translations

--- routers/list_router.py
+++ routers/list_router.py
@@ -11,9 +11,8 @@ from models.list_model import ListModel
 from components.config import Config
 from routers.base_router import BaseRouter
 
-# TODO: Replace with gettext install
-# @see https://stackoverflow.com/questions/14946017/switch-translations-in-python-and-gettext
-_ = gettext.gettext
+translation = gettext.translation('list_router', localedir='i18n', languages=['ru'])
+translation.install()
 
 
 class ListRouter(BaseRouter):
